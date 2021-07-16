125d75b20a239b66c7328f39b801cc5573a9aaa5
Lasoli <l.lipke@yahoo.com>
2021-07-16 19:58:43 +0200

    connecting database attempt 1

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
 const app = express();
+var cors = require("cors");
 const userRouter = require("./src/routes/user.js");
 const messageRouter = require("./src/routes/tweet.js");
 const port = 3000;
-const config = require("./database");
 const mongoose = require("mongoose");
 
-// app.use("/api/user", userRouter);
+const db = require("./database");
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+app.use(cors());
+app.use(express.urlencoded());
+app.use(express.json());
+app.use("/api/user", userRouter);
 // app.use("/api/message", messageRouter);
 
 app.listen(port, () => {
